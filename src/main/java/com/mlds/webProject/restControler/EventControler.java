47c33395b9465f157e737b3a16e8d4f46800f40a
hassaine <hassaine24@gmail.com>
2020-11-29 16:35:11 +0100

    add EventControler

---
+++ src/main/java/com/mlds/webProject/restControler/EventControler.java
@@ -0,0 +1,37 @@
+package com.mlds.webProject.restControler;
+
+import com.mlds.webProject.entity.Event;
+import com.mlds.webProject.entity.User;
+import com.mlds.webProject.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EventControler {
+
+    EventRepository eventRepository;
+
+    @Autowired
+    public EventControler(EventRepository eventRepository) {
+        super();
+        this.eventRepository = eventRepository;
+    }
+
+    @GetMapping("/events")
+    public Iterable<Event> getUsers(){
+        return eventRepository.findAll();
+    }
+
+    @PostMapping("/events")
+    @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
+    public void addUser(@RequestBody Event event) throws Exception {
+        eventRepository.save(event);
+    }
+
+
+}
