5340487ae7901c720d24c2641e2852f8e8ebb480
hassaine <hassaine24@gmail.com>
2020-11-29 13:26:33 +0100

    add entity anotation

--- src/main/java/com/mlds/webProject/entity/User.java
+++ src/main/java/com/mlds/webProject/entity/User.java
@@ -1,4 +1,61 @@
 package com.mlds.webProject.entity;
 
+
+import javax.persistence.*;
+
+@Entity
 public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+
+
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Column
+    private String type;
+
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
