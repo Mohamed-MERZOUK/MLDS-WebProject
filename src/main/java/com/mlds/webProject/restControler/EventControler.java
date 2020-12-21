5e9be4a0424ff3c6e57b8acf345acc59a9c676fb
mohamed <mohamed79135@gmail.com>
2020-12-21 19:30:12 +0100

    Add actual user as owner in EventControler

--- src/main/java/com/mlds/webProject/restControler/EventControler.java
+++ src/main/java/com/mlds/webProject/restControler/EventControler.java
@@ -3,7 +3,10 @@ package com.mlds.webProject.restControler;
 import com.mlds.webProject.entity.Event;
 import com.mlds.webProject.entity.User;
 import com.mlds.webProject.repository.EventRepository;
+import com.mlds.webProject.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventControler {
 
     EventRepository eventRepository;
+    UserRepository userRepository;
 
     @Autowired
-    public EventControler(EventRepository eventRepository) {
+    public EventControler(EventRepository eventRepository, UserRepository userRepository) {
         super();
         this.eventRepository = eventRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/events")
@@ -30,6 +35,16 @@ public class EventControler {
     @PostMapping("/events")
     @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
     public void addEvent(@RequestBody Event event) throws Exception {
+
+        //get the actual user
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentPrincipalName = authentication.getName();
+        User user= userRepository.findByUsername(currentPrincipalName);
+
+        //add the actual user as owner of the event
+        event.setOwner(user);
+
+        //persist the event
         eventRepository.save(event);
     }
 
